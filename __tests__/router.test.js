e76b6520c5428fb983f591f9a1e9c2f5071cb85a
claudia <chandoyo@ucsd.edu>
2021-05-19 23:50:50 -0700

    part 1 coding

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,40 @@
  * @jest-environment jsdom
  */
 
+import {pushToHistory} from '../scripts/router.js';
+
+// use describe to group tests together
+describe('testing pushToHistory', () => {
+
+    // test is to run an individual test
+    // You should have at least one test block for every branch logic (conditional) you have in the function. Name your tests according to each branch.
+    //test if the length of the history stack and the current state object is correct
+
+    test('test settings', () => {
+        pushToHistory('settings',0);
+        // expect(history.state).toStrictEqual({ page: 'settings' });  // this line & line below do same thing 
+        expect(history.state.page).toBe('settings');
+        expect(history.length).toBe(2); // because it will be the 2nd page we visit. the original page we were on would be 1st
+      });
+
+    test('test entries', () => {
+        pushToHistory('entry',1);
+        expect(history.state.page).toBe('entry1');
+        expect(history.length).toBe(3); 
+
+        pushToHistory('entry', 10);
+        expect(history.state.page).toBe('entry10');
+        expect(history.length).toBe(4); 
+    });
+
+    test('test default case, undefined', () => {
+        pushToHistory('undefined case!!',1);
+        // state left empty, gives undefined
+        expect(history.state.page).toBe(undefined);
+        expect(history.length).toBe(5); 
+
+      });
+
+
+
+});
--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,1 +1,19 @@
 // sum.test.js
+
+test('adds 1 + 2 to equal 3', () => {
+
+    expect(1 + 2).toBe(3);
+
+});
+
+
+
+import { sum } from '../scripts/sum.js';
+
+// Now, we can write another test calling the sum() function to test its output:
+
+test('adds 1 + 2 to equal 3', () => {
+
+    expect(sum(1,2)).toBe(3);
+
+});
